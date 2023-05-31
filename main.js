48348655667509d57a631c96fe9706ba04ce5902
LedgendofAll <126633693+LedgendofAll@users.noreply.github.com>
2023-05-30 20:06:59 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var e = 0;
+
+function c() {
+    e = e + 1;
+    document.getElementById("count").innerHTML = e;
+    if (e > 99) {
+        document.getElementById("rev").innerHTML = "Add me! https://web.roblox.com/users/2981487992/profile#!/about";
+    }
+}
